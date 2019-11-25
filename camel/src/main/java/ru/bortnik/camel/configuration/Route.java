88dbe61c6084695446f5963485c16ce8a290954e
bortnik <bort.dm@gmail.com>
2019-11-26 02:59:34 +0500

    some comments

--- camel/src/main/java/ru/bortnik/camel/configuration/Route.java
+++ camel/src/main/java/ru/bortnik/camel/configuration/Route.java
@@ -31,26 +31,33 @@ public class Route {
                         .handled(true)
                         .log(ERROR, log, "${exception.message}: ${exception.stacktrace}");
 
+                // Поиск *.ready-файла
                 from(String.format("file://%s?include=.*.ready&delete=true", camelProperties.getSourceDirectory()))
                         .log(INFO, log, "Flag file found: ${header.CamelFilePath}")
 
+                        // Поиск соотвутствующего *.txt-файла
                         .log(INFO, log, "Load file: ${file:name.noext}.txt")
                         .pollEnrich().simple(String.format("file://%s?fileName=${file:name.noext}.txt&delete=true", camelProperties.getSourceDirectory()))
 
+                        // Валдиация JSON
                         .log(INFO, log, "Validate file body via JSON-schema")
                         .to(String.format("json-validator:%s", camelProperties.getJsonSchema()))
 
+                        // Перенос файла в целевую директорию
                         .log(INFO, log, String.format("Move file to %s/${header.CamelFileName}", camelProperties.getTargetDirectory()))
                         .to(String.format("file://%s", camelProperties.getTargetDirectory()))
 
+                        // Преобразование в объект
                         .log(INFO, log, "Convert JSON ${body} to POJO")
                         .convertBodyTo(SubscriberDto.class)
 
+                        // Запрос места работы по имени и фамилии
                         .log(INFO, log, "Query work info for " + "${body.lastname}" + " " + "${body.firstname}")
                         .to("sql: select * from work_place " +
                                 "where lastname = :#${body.lastname} and firstname = :#${body.firstname}" +
                                 "?outputHeader=workMap")
 
+                        // Заполнение поля work, если возможно
                         .log(INFO, log, "Check subscriber's work info")
                         .choice()
                             .when(simple("${header.workMap.size()} > 0"))
@@ -61,9 +68,11 @@ public class Route {
                                 .log(INFO, log, "Work for ${body.lastname} ${body.firstname} is empty")
                         .end()
 
+                        // Обновление файла в целевой директории
                         .log(INFO, log, String.format("Update file %s/${header.CamelFileName}", camelProperties.getTargetDirectory()))
                         .to(String.format("file://%s", camelProperties.getTargetDirectory()))
 
+                        // Сохранение в БД
                         .log(INFO, log, "Save ${body} to database")
                         .bean(SubscriberService.class, "save")
 
